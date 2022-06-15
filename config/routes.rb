c1df8e98940e201ca73933b9dd05abc45237b496
VuDej <drvu47@gmail.com>
2022-06-15 08:53:58 +0200

    created sessions and registrations Controlelrs and configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  devise_for :users
-  
-  
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
