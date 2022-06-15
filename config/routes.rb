8c43d39a8a5f78dba0f25a4f42593abd8394d68e
VuDej <drvu47@gmail.com>
2022-06-15 12:02:49 +0200

    created current-user controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'current_user/index'
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -8,4 +9,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  get '/current_user', to: 'current_user#index'
 end
