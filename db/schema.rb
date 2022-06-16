e36c94d09834387190cde8f9b1832a35fcca3424
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-17 00:45:41 +0200

    Generate the schema with: add the reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_145726) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_224019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_145726) do
     t.bigint "user_id"
     t.string "car_model"
     t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "pick_up_day"
+    t.date "return_day"
+    t.string "pick_up_city"
+    t.string "return_city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "car_id"
   end
 
   create_table "users", force: :cascade do |t|
