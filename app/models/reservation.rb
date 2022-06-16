f6b01358e9b892dc030e66a87658f4a025e00742
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-17 00:56:38 +0200

    Add validation to the Reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :user, class_name: 'User'
   belongs_to :Car, class_name: 'Car'
+
+  validates :pick_up_day, presence: true
+  validates :return_day, presence: true
+  validates :pick_up_city, presence: true, length: { maximum: 25 }
+  validates :return_city, presence: true, length: { maximum: 25 }
 end
