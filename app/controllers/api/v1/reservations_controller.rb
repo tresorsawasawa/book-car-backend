a346db2e6a214496a107af69f510c7a1ed6d2fe5
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-17 04:02:14 +0200

    Fix linters erros

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::ReservationsController < ApplicationController
   before_action :authorize_request, only: %i[index create destroy]
 
-
   def index
     @reservations = current_user.reservetions
 
