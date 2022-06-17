3ed134f5ca4c09fc51e7f925b03d7c25107d86f9
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-17 02:28:06 +0200

    Add the logic in the controller: create method

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -6,4 +6,20 @@ class Api::V1::ReservationsController < ApplicationController
 
     render json: @reservations, status: 200
   end
+
+  def create
+    @reservation = current_user.reservetions.new(reservation_params)
+
+    if @reservation.save
+      render :create, status: :created
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:pick_up_day, :return_day, :pick_up_city, :return_city, :car_id)
+  end
 end
