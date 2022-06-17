171831a51ebec711f15015ac6d6847e5576d1001
tresorsawasawa <tresorsawasawa@gmail.com>
2022-06-17 13:20:38 +0200

    Fix linters erros

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::ReservationsController < ApplicationController
     @reservations = current_user.reservations
     render json: @reservations, status: 200
   end
+
   def create
     @reservation = current_user.reservations.new(reservation_params)
     if @reservation.save
@@ -12,6 +13,7 @@ class Api::V1::ReservationsController < ApplicationController
       render json: @reservation.errors, status: :unprocessable_entity
     end
   end
+
   def destroy
     if @reservation.destroy
       render json: { message: 'Reservation has been successfully deleted' }
@@ -19,10 +21,13 @@ class Api::V1::ReservationsController < ApplicationController
       render json: @reservation.errors, status: :unprocessable_entity
     end
   end
+
   private
+
   def set_reservation
     @reservation = Reservation.find(params[:id])
   end
+
   def reservation_params
     params.require(:reservation).permit(:pick_up_day, :return_day, :pick_up_city, :return_city, :car_id, :user_id)
   end
