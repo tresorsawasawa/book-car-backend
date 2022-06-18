c52de772b79107f69b0224ab9cd1d72c67b3dd79
VTY1999 <verissimotoyin50@gmail.com>
2022-06-18 13:01:09 +0100

    Test reservation models

--- spec/models/cars_spec.rb
+++ spec/models/cars_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Car, type: :model do
   before(:each) do
-    @car = Car.new(price_per_day: 2000, description: 'This is a car', photo: 'photo.jpeg', car_type: 'jeep', transmission: 'manual')
+    @car = Car.new(price_per_day: 2000, description: 'This is a car', photo: 'photo.jpeg', car_type: 'jeep',
+                   transmission: 'manual')
   end
 
-  it 'is valid if car_model is not nil' do
+  it 'is valid if car_model is nil' do
     @car.car_model = 'Toyota'
     expect(@car).to_not be_valid
   end
@@ -15,22 +16,22 @@ RSpec.describe Car, type: :model do
     expect(@car).to_not be_valid
   end
 
-  it 'is valid if description is nil' do
+  it 'is valid if description is not nil' do
     @car.description = nil
     expect(@car).to_not be_valid
   end
 
-  it 'is valid if photo is nil' do
+  it 'is valid if photo is not nil' do
     @car.photo = nil
     expect(@car).to_not be_valid
   end
 
-  it 'is valid if car_type is nil' do
+  it 'is valid if car_type is not nil' do
     @car.car_type = nil
     expect(@car).to_not be_valid
   end
 
-  it 'is valid if transmission is nil' do
+  it 'is valid if transmission is not nil' do
     @car.transmission = nil
     expect(@car).to_not be_valid
   end
